977d90afa777e492bb9f7911027f7f85b9fcbc2f
Your Name <you@example.com>
2022-10-09 19:08:13 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    float x;
+
+    printf("wprowadz \"x\" \n" );
+    scanf("%f", &x);
+    float y;
+    y=(2*x*x*x)-((4*x*x))+3*x-7;
+    printf("%f \n",y );
+
+    return 0;
+}
